104181ae89deca5953b658099acb442250ca9442
vlades233 <veselovsky_v@ukr.net>
2019-05-15 12:08:38 +0300

    first commit

---
+++ Java_lab_3/Java.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_lab_3;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random random = new Random();
+        int N=5;
+        float array[] = new float[N];
+       float array_new[] = new float[N];
+       for (int i = 0; i < array.length-1; i++) 
+       {
+           array[i] = random.nextFloat();
+       }
+       array[4]=array[2]; // искуственно создал дубликат, что бы наглядно было
+        Arrays.sort(array);
+         System.out.println("Сортированный  массив: " + Arrays.toString(array)); //Сортирую, потому что подумал что я не хочу проходить по всему массиву, лучше пусть он будет проверять прошлый елемент, будет практичнее
+         array_new[0]=array[0];
+        array_new[0]=array[0];
+        for (int i=1;i<array.length;i++){
+            if (array[i-1] != array[i]){
+
+                array_new[i] = array[i];
+            }
+        }
+          System.out.println("Наш массив: " + Arrays.toString(array_new));
+
+        // TODO code application logic here
+    }
+        }
+   
+    
+
+
+
+
+
+
+
+
+ 
+    
